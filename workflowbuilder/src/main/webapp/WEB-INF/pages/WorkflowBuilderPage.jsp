0fc97c11ec4cc0ff0a6af6a16b865cd4d79cd91d
Nikita <npandey@indiana.edu>
2014-04-09 16:08:56 -0400

    Adding the help button for the configure button and the respective nodes. Part of Jira:http://tintin.cs.indiana.edu:8091/browse/PSU-91

--- workflowbuilder/src/main/webapp/WEB-INF/pages/WorkflowBuilderPage.jsp
+++ workflowbuilder/src/main/webapp/WEB-INF/pages/WorkflowBuilderPage.jsp
@@ -1,30 +1,44 @@
 <!doctype html>
 <html lang="en" ng-app="workFlowBuilder">
 <head>
-<meta charset="utf-8" name="viewport" content="width=device-width, initial-scale=1.0">
+<meta charset="utf-8" name="viewport"
+	content="width=device-width, initial-scale=1.0">
 <title>Workflow Editor</title>
 <script type="text/javascript" src="./resources/static/jquery-1.10.2.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.position.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.core.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.widget.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.mouse.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.draggable.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.droppable.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.resizable.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.ui.dialog.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.position.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.core.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.widget.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.mouse.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.draggable.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.droppable.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.resizable.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.ui.dialog.js"></script>
 <script type="text/javascript" src="./resources/static/angular.min.js"></script>
-<script type="text/javascript" src="./resources/static/angular-route.min.js"></script>
-<script type="text/javascript" src="./resources/static/controller/angularController.js"></script>
+<script type="text/javascript"
+	src="./resources/static/angular-route.min.js"></script>
+<script type="text/javascript"
+	src="./resources/static/controller/angularController.js"></script>
 <script type="text/javascript" src="./resources/static/configuration.js"></script>
-<script type="text/javascript" src="./resources/static/jquery.jsPlumb-1.5.5-min.js"></script>
+<script type="text/javascript"
+	src="./resources/static/jquery.jsPlumb-1.5.5-min.js"></script>
 <script type="text/javascript" src="./resources/static/demo-jquery.js"></script>
 <script type="text/javascript" src="./resources/static/demo-list.js"></script>
 <script type="text/javascript" src="./resources/static/bootstrap.js"></script>
-<script type="text/javascript" src="./resources/static/ui-bootstrap-tpls-0.10.0.min.js"></script>
+<script type="text/javascript"
+	src="./resources/static/ui-bootstrap-tpls-0.10.0.min.js"></script>
 <script type="text/javascript" src="./resources/static/bootbox.js"></script>
 <script type="text/javascript" src="./resources/static/custom.js"></script>
 <link rel="stylesheet" type="text/css" href="./resources/css/demo.css">
-<link rel="stylesheet" type="text/css" href="./resources/css/bootstrap.css">
+<link rel="stylesheet" type="text/css"
+	href="./resources/css/bootstrap.css">
 <link rel="stylesheet" type="text/css" href="./resources/css/custom.css">
 
 </head>
@@ -39,8 +53,16 @@
         <div class="modal-body">
 			<form name="form" novalidate>
 				<div ng-repeat="property in properties">
-					<label>{{property.label}}</label> 
-
+					<div id=try style="position:relative;float:left">{{property.label}}
+				<button class="helpFormButton" 
+					id="propertyHelpId" popover= {{property.helpText}}
+					popover-trigger="click" popover-placement="bottom" >
+					<i class=" glyphicon glyphicon-question-sign"
+					style="font-size:15px;"> </i>
+					</button>
+				</div>
+				
+			
 					<div ng-if="isRadioType(property)">
 							<label ng-repeat="option in property.options">
 								<input 	type="radio"	 
@@ -120,44 +142,99 @@
 		<div id="editor-window" class="container">
 			<div class="tool-box ui-widget ui-helper-clearfix">
 				<div class="panel-group" id="accordion">
-  					<div class="panel panel-default">
-    					<div class="panel-heading" data-toggle="collapse" data-parent="accordion" href="#collapseOne">
-    						<h4 class="panel-title">
-      							<a class="accordion-toggle">
-        							Components
-      							</a>
-      							<i class="indicator glyphicon glyphicon-chevron-down  pull-right">
-      							</i>
-      						</h4>
-    					</div>
-    					<div id = "collapseOne" class="panel-collapse collapse in">
-    						<div id = "row" class ="panel-body">
-								<div class="draggable w circle ui-corner-tr ui-widget-content ">
-									<font color="white">Start</font>
-								</div>
-								<div class="draggable w square ui-corner-tr ui-widget-content">
-									<font color="white">Email</font>
-								</div>
-								<br><br>
-								<div class="draggable w rectangle ui-corner-tr ui-widget-content">
+					<div class="panel panel-default">
+						<div class="panel-heading" data-toggle="collapse"
+							data-parent="accordion" href="#collapseOne">
+							<h4 class="panel-title">
+								<a class="accordion-toggle"> Components </a> <i
+									class="indicator glyphicon glyphicon-chevron-down  pull-right">
+								</i>
+							</h4>
+						</div>
+						<div id="collapseOne" class="panel-collapse collapse in">
+							<div id="row" class="panel-body">
+									<div class="draggable w circle ui-corner-tr ui-widget-content ">
+										<font color="white">Start</font>
+									</div>
+									<button class="helpButton " style="float: right; display:inline"
+										id="startHelpId"
+										popover="The Start node can have only one outgoing connection. This node is the initiator node"
+										popover-trigger="focus">
+										<i class=" glyphicon glyphicon-question-sign pull-left"
+											style="font-size: 20px"> </i>
+									</button>
+									<br>
+									<br>
+								
+									<div class="draggable w square ui-corner-tr ui-widget-content">
+										<font color="white">Email</font>
+									</div>
+									<button class="helpButton " style="float: right"
+										id="emailHelpId"
+										popover="The Email node can have one incoming connection and one outgoing connection. This node contains the source and the target email address"
+										popover-trigger="focus">
+										<i class=" glyphicon glyphicon-question-sign pull-left"
+											style="font-size: 20px"> </i>
+									</button>
+								<br>
+								<br>
+								<div
+									class="draggable w rectangle ui-corner-tr ui-widget-content">
 									<font color="white">Request</font>
 								</div>
+								<button class="helpButton " style="float: right"
+									id="requestHelpId"
+									popover="The Request node cam have one incoming connection and one outgoing connection. This node contains the mandatory route and the target final approval"
+									popover-trigger="focus">
+									<i class=" glyphicon glyphicon-question-sign pull-left"
+										style="font-size: 20px"> </i>
+								</button>
+								<br>
+								<br>
+								
 								<div class="draggable w oval ui-corner-tr ui-widget-content">
 									<font color="white">Simple</font>
 								</div>
-								<br><br>
-								<div class="draggable w rectangleCoral r ui-corner-tr ui-widget-content">
+								<button class="helpButton " style="float: right"
+									id="simpleHelpId"
+									popover="The Simple node cam have one incoming connection and one outgoing connection. This node contains the mandatory route and the target final approval"
+									popover-trigger="focus" container: 'body'>
+									<i class=" glyphicon glyphicon-question-sign pull-left"
+										style="font-size: 20px"> </i>
+								</button>
+								<br>
+								<br>
+								<div
+									class="draggable w rectangleCoral r ui-corner-tr ui-widget-content">
 									<font color="white">Role</font>
 								</div>
-								<div class="draggable w ovalMagenta r ui-corner-tr ui-widget-content">
+								<button class="helpButton " style="float: right"
+									id="roletHelpId"
+									popover="The Role node cam have one incoming connection and one outgoing connection. This node contains the type and style"
+									popover-trigger="focus"popover-container: "body">
+									<i class=" glyphicon glyphicon-question-sign pull-left"
+										style="font-size: 20px"> </i>
+								</button>
+								<br>
+								<br>
+								
+								<div
+									class="draggable w ovalMagenta r ui-corner-tr ui-widget-content">
 									<font color="white">NoOp</font>
 								</div>
+								<button class="helpButton " style="float: right" id="noOpHelpId"
+									popover="The NoOp node cam have one incoming connection and one outgoing connection. This node contains only a name and a type"
+									popover-trigger="focus">
+									<i class=" glyphicon glyphicon-question-sign pull-left"
+										style="font-size: 20px"> </i>
+								</button>
 							</div>
 						</div>
 					</div>
-  				</div>
-  			</div>			
-			<div class="drop-area statemachine-demo ui-widget-content ui-state-default"></div>
+				</div>
+			</div>
+			<div
+				class="drop-area statemachine-demo ui-widget-content ui-state-default"></div>
 		</div>
 		<button class="btn btn-primary" id="exportId">Export</button>
 	</div>
--- workflowbuilder/src/main/webapp/resources/static/configuration.js
+++ workflowbuilder/src/main/webapp/resources/static/configuration.js
@@ -1,206 +1,234 @@
 var nodeConfigurationNotAddedError = "Error:Please define nodes configuration on configuration file for corresponding node type";
 
 var nodesConfiguration = {
-		"circle" : {
-			"name" : "requests",
-			"image" : "circle",
-			"header" : "Action Configuration",
-			"properties" : {
-				"name" : {
-					"label" : "Name*",
-					"type" : "text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
-				"mandatoryRoute" : {
-					"label" : "Mandatory Route",
-					"type" : "radio",
-					"options" : [ "True", "False" ]
-				},
-				"activationType" : {
-					"label" : "Activation Type",
-					"type" : "select",
-					"options" : [ "Parallel", "serial" ]
-				},
-				"finalApproval" : {
-					"label" : "Final Approval",
-					"type" : "radio",
-					"options" : [ "True", "False" ]
-				}
-
+	"circle" : {
+		"name" : "requests",
+		"image" : "circle",
+		"header" : "Action Configuration",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
+			"mandatoryRoute" : {
+				"label" : "Mandatory Route",
+				"type" : "radio",
+				"options" : [ "True", "False" ],
+				"helpText" : "Define the mandatory Route"
+			},
+			"activationType" : {
+				"label" : "Activation Type",
+				"type" : "select",
+				"options" : [ "Parallel", "serial" ],
+				"helpText" : "Define the activation Type"
+			},
+			"finalApproval" : {
+				"label" : "Final Approval",
+				"type" : "radio",
+				"options" : [ "True", "False" ],
+				"helpText" : "Define the final approval"
 			}
-		},
-		"rectangle" : {
-			"name" : "requests",
-			"image" : "rectangle",
-			"header" : "Action Configuration",
-			"properties" : {
-				"name" : {
-					"label" : "Name*",
-					"type" : "text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
 
-				"activationType" : {
-					"label" : "Activation Type",
-					"type" : "select",
-					"options" : [ "Parallel", "serial" ]
-				},
-				"mandatoryRoute" : {
-					"label" : "Mandatory Route",
-					"type" : "radio",
-					"options" : [ "True", "False" ]
-				},
-				"ruleTemplate" : {
-					"label" : "Rule Template",
-					"type" : "text",
-					"edit" : "Yes"
-				},
-				"finalApproval" : {
-					"label" : "Final Approval",
-					"type" : "radio",
-					"options" : [ "True", "False" ]
-				}
+		}
+	},
+	"rectangle" : {
+		"name" : "requests",
+		"image" : "rectangle",
+		"header" : "Action Configuration",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
 
+			"activationType" : {
+				"label" : "Activation Type",
+				"type" : "select",
+				"options" : [ "Parallel", "serial" ],
+				"helpText" : "Define the activation Type"
+			},
+			"mandatoryRoute" : {
+				"label" : "Mandatory Route",
+				"type" : "radio",
+				"options" : [ "True", "False" ],
+				"helpText" : "Define the mandatory route"
+			},
+			"ruleTemplate" : {
+				"label" : "Rule Template",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the rule template"
+			},
+			"finalApproval" : {
+				"label" : "Final Approval",
+				"type" : "radio",
+				"options" : [ "True", "False" ],
+				"helpText" : "Define the final approval"
 			}
-		},
-		"square" : {
-			"name" : "requests",
-			"image" : "square",
-			"header" : "Test.Submission.Email",
-			"properties" : {
-				"name" : {
-					"label" : "Name*",
-					"type" : "text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
-				"from" : {
-					"label" : "From",
-					"type" : "text",
-					"defaultVal" : "jawbenne@iu.edu",
-					"edit" : "Yes"
-				},
-				"to" : {
-					"label" : "To",
-					"type" : "text",
-					"defaultVal" : "pacharya@indiana.edu",
-					"edit" : "Yes"
-				},
-				"testAddress" : {
-					"label" : "Test Address",
-					"type" : "text",
-					"defaultVal" : "jawbenne@iu.edu",
-					"edit" : "Yes"
-				},
-				"style" : {
-					"label" : "Style",
-					"type" : "text",
-					"defaultVal" : "Test.Admin.Approval.Email",
-					"edit" : "Yes"
-				},
-				"type" : {
-					"label" : "Type",
-					"type" : "text",
-					"defaultVal" : "org.kuali.rice.kew.mail.EmailNode",
-					"edit" : "No"
-				}
-			}
-		},
 
-		"oval" : {
-			"name" : "requests",
-			"image" : "oval",
-			"header" : "Simple Node",
-			"properties" : {
-				"name":{
-					"label": "Name*",
-					"type":"text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
-				"from" : {
-					"label" : "From",
-					"type" : "text",
-					"defaultVal" : "jawbenne@iu.edu",
-					"edit" : "Yes"
-				},
-				"to" : {
-					"label" : "To",
-					"type" : "text",
-					"defaultVal" : "npandey@indiana.edu",
-					"edit" : "Yes"
-				},
-				"testAddress" : {
-					"label" : "Test Address",
-					"type" : "text",
-					"defaultVal" : "jawbenne@iu.edu",
-					"edit" : "Yes"
-				},
-				"style" : {
-					"label" : "Style",
-					"type" : "text",
-					"defaultVal" : "Test.Admin.Approval.Email",
-					"edit" : "Yes"
-				},
-				"type" : {
-					"label" : "Type",
-					"type" : "text",
-					"defaultVal" : "org.kuali.rice.kew.mail.EmailNode",
-					"edit" : "Yes"
-				}
+		}
+	},
+	"square" : {
+		"name" : "requests",
+		"image" : "square",
+		"header" : "Test.Submission.Email",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
+			"from" : {
+				"label" : "From",
+				"type" : "text",
+				"defaultVal" : "jawbenne@iu.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the senders email address"
+			},
+			"to" : {
+				"label" : "To",
+				"type" : "text",
+				"defaultVal" : "pacharya@indiana.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the recipients email address"
+			},
+			"testAddress" : {
+				"label" : "Test Address",
+				"type" : "text",
+				"defaultVal" : "jawbenne@iu.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the test email address"
+			},
+			"style" : {
+				"label" : "Style",
+				"type" : "text",
+				"defaultVal" : "Test.Admin.Approval.Email",
+				"edit" : "Yes",
+				"helpText" : "Define the style"
+			},
+			"type" : {
+				"label" : "Type",
+				"type" : "text",
+				"defaultVal" : "org.kuali.rice.kew.mail.EmailNode",
+				"edit" : "No",
+				"helpText" : "Defines the type"
 			}
-		},
+		}
+	},
 
-		"rectangleCoral" : {
-			"name" : "requests",
-			"image" : "rectangleCoral",
-			"header" : "Role Based Routing",
-			"properties" : {
-				"name":{
-					"label": "Name*",
-					"type":"text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
-				"qualifierResolverClass" : {
-					"label" : "Qualifier Resolver Class",
-					"type" : "text",
-					"edit" : "Yes"
-				},
-				"activationType" : {
-					"label" : "Activation Type",
-					"type" : "select",
-					"options" : [ "Parallel", "serial" ]
-				}
+	"oval" : {
+		"name" : "requests",
+		"image" : "oval",
+		"header" : "Simple Node",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
+			"from" : {
+				"label" : "From",
+				"type" : "text",
+				"defaultVal" : "jawbenne@iu.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the senders email address"
+			},
+			"to" : {
+				"label" : "To",
+				"type" : "text",
+				"defaultVal" : "npandey@indiana.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the recipients email address"
+			},
+			"testAddress" : {
+				"label" : "Test Address",
+				"type" : "text",
+				"defaultVal" : "jawbenne@iu.edu",
+				"edit" : "Yes",
+				"helpText" : "Define the test email address"
+			},
+			"style" : {
+				"label" : "Style",
+				"type" : "text",
+				"defaultVal" : "Test.Admin.Approval.Email",
+				"edit" : "Yes",
+				"helpText" : "Define the style"
+			},
+			"type" : {
+				"label" : "Type",
+				"type" : "text",
+				"defaultVal" : "org.kuali.rice.kew.mail.EmailNode",
+				"edit" : "Yes",
+				"helpText" : "Define the type"
+			}
+		}
+	},
 
+	"rectangleCoral" : {
+		"name" : "requests",
+		"image" : "rectangleCoral",
+		"header" : "Role Based Routing",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
+			"qualifierResolverClass" : {
+				"label" : "Qualifier Resolver Class",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the Qualifier Resolver Class"
+			},
+			"activationType" : {
+				"label" : "Activation Type",
+				"type" : "select",
+				"options" : [ "Parallel", "serial" ],
+				"helpText" : "Define the activation type"
+				
 			}
-		},
-		"ovalMagenta":{
-			"name" : "requests",
-			"image" : "ovalMagenta",
-			"header" : "NoOp",
-			"properties" : {
-				"name" : {
-					"label" : "Name*",
-					"type" : "text",
-					"edit" : "Yes",
-					"required" : "Yes",
-					"message" : "Name is required."
-				},
-				"type" : {
-					"label" : "Type",
-					"type" : "text",
-					"defaultVal" : "org.kuali.rice.kew.engine.node.NoOpNode",
-					"edit" : "No"
-				}
+
+		}
+	},
+	"ovalMagenta" : {
+		"name" : "requests",
+		"image" : "ovalMagenta",
+		"header" : "NoOp",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Define the name"
+			},
+			"type" : {
+				"label" : "Type",
+				"type" : "text",
+				"defaultVal" : "org.kuali.rice.kew.engine.node.NoOpNode",
+				"edit" : "No",
+				"helpText" : "Define the type"
 			}
+		}
+
 		},
 		"configure" : {
 			"image" : "configure",
@@ -281,7 +309,99 @@ var nodesConfiguration = {
 					"required" : "Yes",
 					"message" : "Name Space is required."
 				}
+
+			}
+	},
+	"configure" : {
+		"image" : "configure",
+		"header" : "Workflow Configuration",
+		"properties" : {
+			"name" : {
+				"label" : "Name*",
+				"type" : "text",
+				"edit" : "Yes",
+				"required" : "Yes",
+				"message" : "Name is required.",
+				"helpText" : "Name is a mandatory field. It is required to be filled. This defines the name"
+			},
+			"parent" : {
+				"label" : "Parent",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the parent for the workspace"
+			},
+			"description" : {
+				"label" : "Description",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Give a description for your workspace"
+			},
+			"label" : {
+				"label" : "Label",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Label is used to give a label for the configuration xml"
+			},
+			"postProcessor" : {
+				"label" : "Post Processor",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the post Processor for the workspace"
+			},
+			"superUserGroup" : {
+				"label" : "Super User Group",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the super User Group for the workspace"
+			},
+			"blanketApprovePolicy" : {
+				"label" : "Blanket Approve Policy",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "blanket Approve Policy needs to be given"
+			},
+			"reportingGroup" : {
+				"label" : "Reporting Group",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Reporting Group for the workspace"
+			},
+			"defaultExceptionGroup " : {
+				"label" : "Default Exception Group",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "States the default Exception Group for the workspace"
+			},
+			"docHandler" : {
+				"label" : "Doc Handler",
+				"type" : "text",
+				"edit" : "Yes",
+				"helpText" : "Define the doc Handler for the workspace"
+			},
+			"active" : {
+				"label" : "Active",
+				"type" : "radio",
+				"options" : [ "True", "False" ],
+				"helpText" : "Choose the  the workspace to be active or not"
+			},
+			"routingVersion" : {
+				"label" : "Routing Version",
+				"type" : "text",
+				"defaultVal" : "2",
+				"edit" : "No",
+				"helpText" : "This is a non editable field"
+			},
+			"nameSpace" : {
+				"label" : "Name Space*",
+				"type" : "select",
+				"options" : [ "KUALI" ],
+				"required" : "Yes",
+				"message" : "Name Space is required.",
+				"helpText" : "Define the namespace for the workspace"
+
 			}
 		}
+	}
+	
 
 };
